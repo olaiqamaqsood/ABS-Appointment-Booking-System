64f801c1e6bdd219d73ad0c1c1cbae705898adea
Olaiqa Maqsood <olaiqamaqsood@gmail.com>
2026-09-24 13:04:44 +0500

    Fix database connection handling

--- config.py
+++ config.py
@@ -10,7 +10,11 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+)
 
 SessionLocal = sessionmaker(bind=engine)
 
